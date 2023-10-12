73d7738c702c97e601d54990815c75f254567426
polavarapunitin@gmail.com <polavarapunitin@gmail.com>
2023-10-12 15:50:58 -0400

    Implementation of timer class, won't do anything until called in game view class.

--- SudokuLib/Timer.h
+++ SudokuLib/Timer.h
@@ -8,12 +8,18 @@
 #ifndef PROJECT1_335_SUDOKULIB_TIMER_H
 #define PROJECT1_335_SUDOKULIB_TIMER_H
 
-class Timer
+class Timer : public wxWindow
 {
 private:
+    /// The timer that allows for animation
+    wxTimer mTimer;
+    long mTime = 0;
+    /// Stopwatch used to measure elapsed time
+    wxStopWatch mStopWatch;
 
 public:
-
+    Timer();
+    void OnDraw(wxGraphicsContext *dc);
 };
 
 #endif //PROJECT1_335_SUDOKULIB_TIMER_H
