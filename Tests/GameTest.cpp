c101cb5ad9104858f975385ea3a9b0c3f434bf7c
samwycoff <wycoffs3@msu.edu>
2023-10-16 16:23:30 -0400

    Began attempt at testing GameView OnDraw

--- Tests/GameTest.cpp
+++ Tests/GameTest.cpp
@@ -4,3 +4,29 @@
  */
 
 #include <pch.h>
+#include "gtest/gtest.h"
+#include <Game.h>
+#include <regex>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <wx/filename.h>
+#include <memory>
+
+using namespace std;
+
+TEST(GameTest, Construct){
+    Game game;
+}
+
+// Determine if all items are being iterated over and drawn
+TEST(GameTest, OnDraw){
+    Game game;
+
+    // Test that there are no items in the game
+        //add code here
+
+    //shared_ptr<Item> item = make_shared<Item>(&game)
+    //game.Add(item);
+
+}
