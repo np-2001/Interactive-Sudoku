146e19bfb2d6716f08f7df5a3d7364d538f507a8
np-2001 <polavarapunitin@gmail.com>
2023-10-22 14:38:20 -0400

    Fixing ItemTest

--- Tests/ItemTest.cpp
+++ Tests/ItemTest.cpp
@@ -22,38 +22,44 @@ public:
     ItemMock(Game *game, std::shared_ptr<wxImage> image) : Item(game, image) {}
     void Eat(Item *item) {};
     void Accept(VisitorItem* visitor) {};
+    void Update(double elapsed){};
+    void Eat() {};
+    void Draw(std::shared_ptr<wxGraphicsContext> graphics) {};
+    bool EatTest(double x, double y) {};
+    void XmlLoad(wxXmlNode *node) {};
+
 
 };
 
 TEST(ItemTest, Construct) {
     Game game;
-    //ItemMock item(&game, image);
+    ItemMock item(&game, image);
 }
 
 TEST(ItemTest, HitTest) {
     // Create an item to test
     Game game;
-    //ItemMock item(&game, image);
+    ItemMock item(&game, image);
 
     // Give it a location
     // Always make the numbers different, in case they are mixed up
-    //item.SetLocation(100, 200);
+    item.SetLocation(100, 200);
 
     // Center of the item should be a true
-    //ASSERT_TRUE(item.HitTest(100, 200));
-//
-//    // Left of the item
-//    ASSERT_FALSE(item.HitTest(10, 200));
-//
-//    // Right of the item
-//    ASSERT_FALSE(item.HitTest(200, 200));
-//
-//    // Above the item
-//    ASSERT_FALSE(item.HitTest(100, 0));
-//
-//    // Below the item
-//    ASSERT_FALSE(item.HitTest(100, 300));
-//
-//    // On a item transparent pixel
-//    ASSERT_FALSE(item.HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
+    ASSERT_TRUE(item.HitTest(100, 200));
+
+    // Left of the item
+    ASSERT_FALSE(item.HitTest(10, 200));
+
+    // Right of the item
+    ASSERT_FALSE(item.HitTest(200, 200));
+
+    // Above the item
+    ASSERT_FALSE(item.HitTest(100, 0));
+
+    // Below the item
+    ASSERT_FALSE(item.HitTest(100, 300));
+
+    // On a item transparent pixel
+    ASSERT_FALSE(item.HitTest(100 - 125/2 + 17, 200 - 117/2 + 16));
 }
