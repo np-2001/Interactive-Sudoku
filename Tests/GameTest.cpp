71a22fa638dfcb705bad0377def0a8a34aada632
samwycoff <wycoffs3@msu.edu>
2023-10-24 20:57:14 -0400

    Created a Test for the Clear function in game. Tests pass. Clear() works!

--- Tests/GameTest.cpp
+++ Tests/GameTest.cpp
@@ -8,6 +8,7 @@
 #include <Game.h>
 #include <Item.h>
 #include <Digit.h>
+#include <Given.h>
 #include <Sparty.h>
 #include <regex>
 #include <string>
@@ -39,6 +40,44 @@ TEST(GameTest, HitTest){
     ASSERT_EQ(game.HitTest(1000, 1000), nullptr);
 
     ASSERT_TRUE(game.HitTest(100, 200) == digit);
+
+}
+
+TEST(GameTest, Clear){
+    Game game;
+
+    //initialize an image for an item
+    std::shared_ptr<wxImage> image = std::make_shared<wxImage>(L"images/1b.png", wxBITMAP_TYPE_ANY);
+
+    ASSERT_EQ(game.HitTest(100, 200), nullptr) <<
+                                                    L"Testing empty game, no items in mItems.";
+
+    game.GetLevel()->SetLevel(L"Levels/level0.xml");
+    game.GetLevel()->LoadLevel();
+
+    //add digit to mItems
+    shared_ptr<Digit> digit = make_shared<Digit>(&game,image,1);
+    game.Add(digit);
+    digit->SetPixelLocation(100, 200);
+
+    //add given to mItems
+    shared_ptr<Given> given = make_shared<Given>(&game,image,1);
+    game.Add(given);
+    given->SetPixelLocation(300, 400);
+
+
+    ASSERT_TRUE(game.HitTest(100, 200) == digit) <<
+                                               L"Testing digit was added to mItems";
+    ASSERT_TRUE(game.HitTest(300, 400) == given) <<
+                                               L"Testing given was added to mItems";
+
+    // clear mItems of all items
+    game.Clear();
+
+    ASSERT_EQ(game.HitTest(100, 200), nullptr) <<
+                                               L"Testing digit was removed from mItems.";
+    ASSERT_EQ(game.HitTest(300, 400), nullptr) <<
+                                               L"Testing given was removed from mItems.";
 
 }
 
